cd495e3594e4461acafee3a6bef9e591d1917f0d
Anderson Rubio Jucá Filho <andersonrubiojuca@yahoo.com.br>
2022-05-05 23:46:42 -0300

    Criado o UI design

--- app/src/main/java/com/example/bankline_android/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/com/example/bankline_android/ui/statement/BankStatementActivity.kt
@@ -2,8 +2,12 @@ package com.example.bankline_android.ui.statement
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.bankline_android.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bankline_android.databinding.ActivityBankStatementBinding
+import com.example.bankline_android.domain.Correntista
+import com.example.bankline_android.domain.Movimentacao
+import com.example.bankline_android.domain.TipoMovimentacao
+import java.lang.IllegalArgumentException
 
 class BankStatementActivity : AppCompatActivity() {
 
@@ -15,8 +19,27 @@ class BankStatementActivity : AppCompatActivity() {
         ActivityBankStatementBinding.inflate(layoutInflater)
     }
 
+    private val accoutntHolder by lazy {
+        intent.getParcelableExtra<Correntista>(EXTRA_ACCOUNT_HOLDER) ?: throw IllegalArgumentException()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatement()
+    }
+
+    private fun findBankStatement() {
+        val dataSet = ArrayList<Movimentacao>()
+        dataSet.add(Movimentacao(1, "05/05/2022 09:24:55", "LOREN IPSUN", 1000.0, tipo = TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1, "05/05/2022 09:24:55", "LOREN IPSUN", 1000.0, tipo = TipoMovimentacao.DESPESA, 1))
+        dataSet.add(Movimentacao(1, "05/05/2022 09:24:55", "LOREN IPSUN", 1000.0, tipo = TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1, "05/05/2022 09:24:55", "LOREN IPSUN", 1000.0, tipo = TipoMovimentacao.DESPESA, 1))
+        dataSet.add(Movimentacao(1, "05/05/2022 09:24:55", "LOREN IPSUN", 1000.0, tipo = TipoMovimentacao.RECEITA, 1))
+
+        binding.rvBankStatement.adapter = BankStatementAdapter(dataSet)
     }
 }
